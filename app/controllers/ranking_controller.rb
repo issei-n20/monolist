55022ebd12eebfe898d190e62c279b26ac1d9b44
issei_n20 <nema.irm@gmail.com>
2015-10-21 07:29:38 +0000

    ranking 1021

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,8 +1,11 @@
 class RankingController < ApplicationController
   def have
-    @have_ranking_item = Have.group(:item_id).order('item_id desc').count('item_id').keys
+    @have_item = Item.all
+    @have_ranking_item = Have.group('item_id').order('count_item_id desc').limit(10).count('item_id').keys
   end
 
   def want
+    @want_item = Item.all
+    @want_ranking_item = Want.group('item_id').order('count_item_id desc').limit(10).count('item_id').keys
   end
 end
